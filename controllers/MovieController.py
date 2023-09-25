8671a87a8747ad2164a5bf3c1d651abcbde1f1e4
LuisSebs <sebastian_luis@ciencias.unam.mx>
2023-09-24 20:44:32 -0600

    inicio

--- controllers/MovieController.py
+++ controllers/MovieController.py
@@ -1,8 +1,21 @@
 from flask import Blueprint, request, render_template, redirect, url_for, flash
-
+from model.model_peliculas import add_movie
 movie = Blueprint('movie', __name__, url_prefix='/movie')
 
 @movie.route('/')
 def main_view_movie_controller():
     return render_template('pelicula.html')
 
+@movie.route('/registrar', methods=('GET', 'POST'))
+def registrar_pelicula():
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        genero = request.form['genero']
+        duracion = request.form['duracion']
+        inventario = request.form['inventario']
+        status = add_movie(nombre,genero,duracion,inventario)
+        if status:
+            return 'OK'
+        else:
+            return 'ERROR'
+    return render_template('user-registration.html')
--- model/model_peliculas.py
+++ model/model_peliculas.py
@@ -1,3 +1,4 @@
+from alchemyClasses import db
 from alchemyClasses.Pelicula import Pelicula
 
 def get_all_peliculas():
@@ -5,3 +6,8 @@ def get_all_peliculas():
 
 def get_pelicula_by_id(id):
     return Pelicula.query.filter(Pelicula.idPelicula == id)
+
+def add_movie(name, gender, duration, stock):
+    nueva_pelicula = Pelicula(name, gender, duration, stock)
+    db.session.add(nueva_pelicula)
+    db.session.commit()
