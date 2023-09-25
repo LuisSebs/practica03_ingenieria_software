dad693f12641567a3bebb88214df8a6c041650f9
LuisSebs <sebastian_luis@ciencias.unam.mx>
2023-09-24 20:27:26 -0600

    CRUD usuario una sola vista

--- controllers/UserController.py
+++ controllers/UserController.py
@@ -11,7 +11,7 @@ user = Blueprint('user', __name__, url_prefix='/user')
 
 @user.route('/')
 def main_view_user_controller():
-    return render_template('usuario.html')
+    return render_template('usuario.html', usuarios = get_all_users())
 
 @user.route('/registrar', methods=('GET', 'POST'))
 def registrar_usuario():
@@ -26,46 +26,23 @@ def registrar_usuario():
             return 'ERROR'
     return render_template('user-registration.html')
 
-@user.route('/leer')
-def leer_usuario():
-    usuarios = get_all_users()
-    tabla = "<table>\n"
-    for usuario in usuarios:
-        tabla += "<tr>\n"
-        tabla += "<td>{}</td><td>{}</td><td>{}</td>\n".format(usuario.idUsuario, usuario.nombre, usuario.email)
-        tabla += "</tr>\n"
-    tabla += "</table>"
-    return tabla
-
-@user.route('/actualizar', methods=('GET', 'POST'))
-def actualizar_usuario():
+@user.route('/actualizar/<string:id>', methods=('GET', 'POST'))
+def actualizar_usuario(id):
     if request.method == 'POST':
-        id_user = request.form['id']
-        nuevo_nombre = request.form['nombre']
-        nuevo_email = request.form['correo']
-        update_user_name(id_user,nuevo_nombre)
-        update_user_email(id_user, nuevo_email)
-        return f"id={id_user}<br/>nuevo_nombre={nuevo_nombre}<br/> {nuevo_email}"
+        id_user = request.form['id'];
+        nombre = request.form['nombre']
+        correo = request.form['correo']
+        status1 = update_user_name(id_user, nombre)
+        status2 = update_user_email(id_user, correo)
+        return redirect(url_for('user.main_view_user_controller'))
     else:
-        id_usuario = request.args.get('id')
-        if id_usuario:
-            usuario = get_user_by_id(int(id_usuario))
-            # Si no existe el usuario
-            if not usuario:
-                # Enviamos mensaje de usuario no encontrado
-                flash('Usuario no encontrado')
-            return  render_template('user-update.html', usuario = usuario)
-        else:
-            return render_template('user-update.html')
-
-@user.route('/eliminar', methods = ('GET', 'POST'))
-def eliminar_usuario():
-    if request.method == 'POST':
-        id_user = request.form['id']
-        status = delete_user(id_user)
-        return 'Deleted'
-    else:
-        return render_template('user-delete.html')
+        usuario = get_user_by_id(int(id))
+        return render_template('usuario.html', usuarios = get_all_users(), usuario=usuario)
+    
+@user.route('/eliminar/<string:id>', methods = ('GET', 'POST'))
+def eliminar_usuario(id):
+    status = delete_user(id)
+    return redirect(url_for('user.main_view_user_controller'))
 
 @user.route('/error-message')
 def example_flash():
